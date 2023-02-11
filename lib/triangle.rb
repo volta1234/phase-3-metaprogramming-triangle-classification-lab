20aa65fa6716536df58fe3d3dc6443c733a186f4
CALVINCE <calvince.ogwel@gmail.com>
2023-02-11 16:11:28 +0300

    completed graded lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+    raise TriangleError if [a, b, c].any? { |side| side <= 0 }
+    raise TriangleError if (a+b<=c) || (a+c<=b) || (b+c<=a)
+
+    return :equilateral if a == b && b == c
+    return :isosceles if a == b || a == c || b == c
+    :scalene
+  end
 end
+
+triangle1 = Triangle.new(0,0,0)
